185478b77d11c35188a9b5e9e7c51a76f9e774a5
Arju <iaat@pop-os.localdomain>
2021-05-13 09:17:58 +0530

    smart_contract_tests_updated

---
+++ contracts/TokenCrowdSale.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+
+contract TokenCrowdSale is Crowdsale {
+
+    constructor(uint256 _rate, address payable _wallet, IERC20 _token) Crowdsale(_rate, _wallet, _token) public {
+
+
+
+    }
+
+}
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,83 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Success Token", function () {
 
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+  var Success, success; 
+
+  beforeEach('Token Deployment', async() => {
+
+      Success = await ethers.getContractFactory("Success");
+      success = await Success.deploy("Success","SCS",18);
+
+  })
+
+  describe('Token Parameters', async()=> {
+
+    it("Token Name Matches", async function () {
+  
+      await success.deployed();
+  
+      expect(await success.name()).to.equal("Success");
+
+    });
+
+    it("Token Symbol Matches", async function () {
+  
+      await success.deployed();
+  
+      expect(await success.symbol()).to.equal("SCS");
+
+    });
+
+    it("Token Decimal Matches", async function () {
+  
+      await success.deployed();
+  
+      expect(await success.decimals()).to.equal(18);
+
+    });
+
+  })
+
 });
+
+
+describe("Crowdsale", function() {
+
+  var CrowdSale, crowdsale, Token, token;
+
+  beforeEach('Crowdsale Deployment', async()=>{
+
+    [deployer, wallet] = await ethers.getSigners();
+
+    // token
+    Token = await ethers.getContractFactory("Success");
+    token = await Token.deploy("Success","SCS",18);
+
+    // crowdsale
+    CrowdSale = await ethers.getContractFactory('TokenCrowdSale');
+    crowdsale = await CrowdSale.deploy(500, wallet.address, token.address);
+
+  })
+
+  describe('CrowdSale parameters',async() => {
+    
+    it('Token Address Matches',async() => {
+      const tokens = await crowdsale.token();
+      expect(tokens).to.equal(token.address);
+    })
+
+    it('Rate Matches',async() => {
+      const rate = await crowdsale.rate();
+      expect(rate).to.equal(500);
+    })
+
+    it('Wallet Matches',async() => {
+      const wallets = await crowdsale.wallet();
+      expect(wallets).to.equal(wallet.address);
+    })
+
+  })
+
+})
