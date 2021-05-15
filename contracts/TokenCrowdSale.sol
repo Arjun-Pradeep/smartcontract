f79a4f09a9b957d5e55ea77f14f59e504b4b072e
Arju <iaat@pop-os.localdomain>
2021-05-15 13:03:03 +0530

    crowdsale_updated

--- contracts/TokenCrowdSale.sol
+++ contracts/TokenCrowdSale.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
@@ -9,6 +10,7 @@ import "@openzeppelin/contracts/crowdsale/validation/WhitelistCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 import "@openzeppelin/contracts/lifecycle/Pausable.sol";
+import "hardhat/console.sol";
 
 contract TokenCrowdSale is
     Crowdsale,
@@ -29,6 +31,13 @@ contract TokenCrowdSale is
     uint256 public airDropWallet = 2;
     uint256 public tokenSaleWallet = 25;
 
+    //Token Address
+    address public reserveWalletAddress;
+    address public interestpayoutAddress;
+    address public teamMemberHRWalletAddress;
+    address public companyGeneralFundWalletAddress;
+    address public airDropWalletAddress;
+
     // Investor Contributions
     uint256 public investorMinCap = 200000000000000; // 0.002 ETH
     uint256 public investorHardCap = 100000000000000000000; // 100 ETH
@@ -46,7 +55,12 @@ contract TokenCrowdSale is
         uint256 _cap,
         uint256 _goal,
         uint256 _openingTime,
-        uint256 _closingTime
+        uint256 _closingTime,
+        address _reserveWalletAddress,
+        address _interestpayoutAddress,
+        address _teamMemberHRWalletAddress,
+        address _companyGeneralFundWalletAddress,
+        address _airDropWalletAddress
     )
         public
         Crowdsale(_rate, _wallet, _token)
@@ -55,6 +69,12 @@ contract TokenCrowdSale is
         RefundableCrowdsale(_goal)
     {
         require(_goal <= _cap);
+        reserveWalletAddress = _reserveWalletAddress;
+        interestpayoutAddress = _interestpayoutAddress;
+        teamMemberHRWalletAddress = _teamMemberHRWalletAddress;
+        companyGeneralFundWalletAddress = _companyGeneralFundWalletAddress;
+        airDropWalletAddress = _airDropWalletAddress;
+        
     }
 
     /**
@@ -87,9 +107,34 @@ contract TokenCrowdSale is
     }
 
     // @dev finalization task
-    function _finalization() internal {
+    function _finalization() internal { 
         if (goalReached()) {
             ERC20Mintable.finishMinting();
+            uint256 totalMinted = ERC20Mintable.totalSupply();
+            console.log("TOTAL MINTED::", totalMinted); 
+            uint256 finalTotalSupply = totalMinted.div(tokenSaleWallet).mul(100);
+            
+            ERC20Mintable.mint(
+                reserveWalletAddress,
+                finalTotalSupply.div(reserveWallet)
+            );
+            ERC20Mintable.mint(
+                interestpayoutAddress,
+                finalTotalSupply.div(interestpayout)
+            );
+            ERC20Mintable.mint(
+                teamMemberHRWalletAddress,
+                finalTotalSupply.div(teamMemberHRWallet)
+            );
+            ERC20Mintable.mint(
+                companyGeneralFundWalletAddress,
+                finalTotalSupply.div(companyGeneralFundWallet)
+            );
+            ERC20Mintable.mint(
+                airDropWalletAddress,
+                finalTotalSupply.div(airDropWallet)
+            );
+
             Pausable.unpause();
         }
         super._finalization();
